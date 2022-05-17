7d023d822f55cacf26d667dd7d938f4ca0c42f78
emagdnimMQT <mark.w.novak@gmail.com>
2022-05-17 14:52:35 -0400

    Added for of and for in loops to userQuery function to make application scalable.

--- main.js
+++ main.js
@@ -1,50 +1,54 @@
-const user1 = {
-    userName: '@elonmusk',
-    displayName: 'Elon Musk',
-    joinedDate: 'June 2009',
-    followingCount: 103,
-    followerCount: 47900000,
-    avatarURL: 'assets/elonmusk.jpg',
-    coverPhotoURL: 'assets/elonmusk-cover.jpeg',
-    tweets: [
-        {
-            text: 'I admit to judging books by their cover',
-            timestamp: '5/6/2022 10:01:20'
-        },
-        {
-            text: 'Starship to the moon',
-            timestamp: '12/09/2021 18:37:12'
-        },
-        {
-            text: 'Out on launch pad, engine swap underway',
-            timestamp: '2/09/2021 12:11:51'
-        }
-    ]
-};
+const twitterUsers = {
+    user1: {
+        userName: '@elonmusk',
+        displayName: 'Elon Musk',
+        joinedDate: 'June 2009',
+        followingCount: 103,
+        followerCount: 47900000,
+        avatarURL: 'assets/elonmusk.jpg',
+        coverPhotoURL: 'assets/elonmusk-cover.jpeg',
+        tweets: [
+            {
+                text: 'I admit to judging books by their cover',
+                timestamp: '5/6/2022 10:01:20'
+            },
+            {
+                text: 'Starship to the moon',
+                timestamp: '12/09/2021 18:37:12'
+            },
+            {
+                text: 'Out on launch pad, engine swap underway',
+                timestamp: '2/09/2021 12:11:51'
+            }
+        ]
+    },
+    user2: {
+        userName: '@BillGates',
+        displayName: 'Bill Gates',
+        joinedDate: 'June 2009',
+        followingCount: 274,
+        followerCount: 53800000,
+        avatarURL: 'assets/billgates.jpg',
+        coverPhotoURL: 'assets/billgates-cover.jpeg',
+        tweets: [
+            {
+                text: 'Everybody asks, how is the next Windows coming along? But nobody asks how is Bill? :/',
+                timestamp: '2/10/2021 00:01:20'
+            },
+            {
+                text: 'Should I start tweeting memes? Let me know in a comment.',
+                timestamp: '2/09/2021 18:37:12'
+            },
+            {
+                text: 'In 2020, I read a book every hour.',
+                timestamp: '2/09/2021 12:11:51'
+            }
+        ]
+    }
+}
 
-const user2 = {
-    userName: '@BillGates',
-    displayName: 'Bill Gates',
-    joinedDate: 'June 2009',
-    followingCount: 274,
-    followerCount: 53800000,
-    avatarURL: 'assets/billgates.jpg',
-    coverPhotoURL: 'assets/billgates-cover.jpeg',
-    tweets: [
-        {
-            text: 'Everybody asks, how is the next Windows coming along? But nobody asks how is Bill? :/',
-            timestamp: '2/10/2021 00:01:20'
-        },
-        {
-            text: 'Should I start tweeting memes? Let me know in a comment.',
-            timestamp: '2/09/2021 18:37:12'
-        },
-        {
-            text: 'In 2020, I read a book every hour.',
-            timestamp: '2/09/2021 12:11:51'
-        }
-    ]
-};
+
+
 
 // declare variables for dom elements
 const headerCtr = document.getElementById('header-ctr');
@@ -55,17 +59,23 @@ const tweetsCtr = document.getElementById('tweets-ctr');
 
 // URL search params and query string
 function userQuery() {
-    const params = new URLSearchParams(window.location.search)
+    let userToDisplay;
+    const params = new URLSearchParams(window.location.search);
     for (const param of params) {
-        if(param[1] === 'user1') {
-            return user1
-        } else if (param[1] === 'user2') {
-            return user2
+        for(let user in twitterUsers) {
+            // console.log(twitterUsers[user])
+            // console.log(user)
+            if(param[1] === user) {
+                userToDisplay = twitterUsers[user];
+            }
         }
+
+        // console.log(userToDisplay)
+        return userToDisplay;
     }   
 }
 
-
+console.log(userQuery())
 
 // create header section
 headerCtr.innerHTML = `
@@ -156,9 +166,9 @@ for (let tweet of userQuery().tweets) {
     const timeAgo = (date) => {
         const timeAgoInSeconds = Math.floor((new Date() - new Date(date)) / 1000);
         const {interval, epoch} = getDuration(timeAgoInSeconds);
-        console.log(interval)
-        console.log(epoch)
-        console.log(date)
+        // console.log(interval)
+        // console.log(epoch)
+        // console.log(date)
         // const suffix = interval === 1 ? '' : 's';
         // return `${interval} ${epoch} ${suffix} ago`;
 
